ef3d4a369ef3a523cd49bd33375b365168663fbd
mehdi ali <103565285+MehdiAliPak@users.noreply.github.com>
2022-11-26 10:01:54 +0330

    Create enqueue.php
    in this file we registered and added styles to our theme

---
+++ includes/front/enqueue.php
@@ -0,0 +1,20 @@
+<?php
+
+function u_enqueue() {
+    wp_register_style(
+        'u_font_rubik_and_pacifico',
+        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap'
+    );
+    wp_register_style(
+        'u-bootstrap_icons',
+        get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+    );
+    wp_register_style(
+        'u-theme',
+        get_theme_file_uri('assets/public/index.css')
+    );
+
+    wp_enqueue_style('u_font_rubik_and_pacifico');
+    wp_enqueue_style('u-bootstrap_icons');
+    wp_enqueue_style('u-theme');
+}
