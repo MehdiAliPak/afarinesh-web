c65e3fdd8fa9ddade6b71ddd4d083fe2e26fe55a
mehdi ali <103565285+MehdiAliPak@users.noreply.github.com>
2022-11-26 11:08:44 +0330

    including head.php
    this file will add some links to the head of our theme

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action('wp_head', 'u_head', 5);
