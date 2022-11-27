2ea8a5fc24efef236c31a67f59524f4a533ae62b
mehdi ali <103565285+MehdiAliPak@users.noreply.github.com>
2022-11-27 12:32:04 +0330

    adding editor.css
    we add this file to remove extra options for search box from site editor of wordpress

--- includes/setup.php
+++ includes/setup.php
@@ -6,6 +6,7 @@ function u_setup_theme () {
     add_editor_style([
         'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
         'assets/bootstrap-icons/bootstrap-icons.css',
-        'assets/public/index.css'
+        'assets/public/index.css',
+        'assets/editor.css'
     ]);
 }
